5784793013d74780f22d39f4e49bbc7a999dbf00
Infinitybeond1 <88919270+Infinitybeond1@users.noreply.github.com>
2022-02-13 20:22:03 +0000

    fix brew install

--- brew.sh
+++ brew.sh
@@ -2,3 +2,5 @@
 
 # A simple script that installs linuxbrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"' >> ${shell_profile}
+eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
