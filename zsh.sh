129a4dd274e8a17abfc54816dd02b836e61d7808
arcolinuxz <arcolinuxinfo@gmail.com>
2022-02-13 17:12:24 -0600

    configure starship prompt

--- zsh.sh
+++ zsh.sh
@@ -4,4 +4,5 @@ sudo pacman -S zsh kitty starship
 touch ~/.zshrc
 echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 chsh -s /usr/bin/zsh
-mkdir -p ~/.config && 
+mkdir -p ~/.config
+cd ~/.config && curl -O https://raw.githubusercontent.com/Infinitybeond1/archsync/main/starship.toml
